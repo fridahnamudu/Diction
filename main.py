531c198faaf854e04a402629d6c75d8f4970d4ac
Fridah Joy Namudu <67759660+fridahnamudu@users.noreply.github.com>
2022-01-16 12:55:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-# Python3 program for a word frequency
-# counter after crawling/scraping a web-page
 import requests
 import bs4
 import operator
 from collections import Counter
 import re
 
-'''Function defining the web-crawler/core
-spider, which will fetch information from
-a given website, and push the contents to
-the second function clean_wordlist()'''
+
 
 
 def start(url):
-    # empty list to store the contents of
-    # the website fetched from our web-crawler
+ 
     wordlist = []
     source_code = requests.get(url).text
-
-    # BeautifulSoup object which will
-    # ping the requested url for data
     soup = bs4(source_code, 'html.parser')
 
-    # Text in given web-page is stored under
-    # the <div> tags with class <entry-content>
+   
     for each_text in soup.findAll('div', {'class': 'entry-content'}):
         content = each_text.text
-
-        # use split() to break the sentence into
-        # words and convert them into lowercase
         words = content.lower().split()
 
         for each_word in words:
@@ -36,7 +23,6 @@ def start(url):
         clean_wordlist(wordlist)
 
 
-# Function removes any unwanted symbols
 
 
 def clean_wordlist(wordlist):
@@ -51,9 +37,6 @@ def clean_wordlist(wordlist):
             clean_list.append(word)
     create_dictionary(clean_list)
 
-
-# Creates a dictionary containing each word's
-# count and top_20 occurring words
 
 
 def create_dictionary(clean_list, url="https://pesapal.freshteam.com/jobs/-z8xM8RCgTx7/junior-developer"):
@@ -80,17 +63,10 @@ def create_dictionary(clean_list, url="https://pesapal.freshteam.com/jobs/-z8xM8
             if unique_word[element] == 1:
                 print(element)
 
-        # starts crawling and prints output
+        
         start(url)
 
 
-'''
-To get the count of each word in the crawled page -->
 
-# operator.itemgetter() takes one
-# parameter either 1(denotes keys)
-# or 0 (denotes corresponding values)for key, value in sorted(word_count.items(), key = operator.itemgetter(1)):
-print ("% s : % s " % (key, value))<-- 
-'''
 
-# Driver code
+
